a91fa66cb2e4fad498319266b25241709f143161
redscips <redscipher@gmail.com>
2025-04-27 17:10:42 -0300

    feat: definindo a rota login

--- frente-social/src/app/app.routes.ts
+++ frente-social/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+//importacoes: componentes
+import { LoginComponent } from './componentes/login/login.component';
 
-export const routes: Routes = [];
+//rotas
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    //redireciona p/ a pagina de login caso a rota seja vazia
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    //caso rota nao encontrada, redireciona p/ login
+    path: '**',
+    redirectTo: 'login'
+  }
+];

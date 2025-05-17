b672fdea7fd9669b01e86313bfdd9b56325ed04a
redscips <redscipher@gmail.com>
2025-05-17 14:30:32 -0300

    feat: componentes de entrar e cadastrar

--- frente-social/src/app/componentes/login/login.component.ts
+++ frente-social/src/app/componentes/login/login.component.ts
@@ -1,95 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-//importacoes: tipos
-import { tUsuario } from '../../tipos/comuns';
-//componentes
-import { CreEntradaComponent } from '../cre-entrada/cre-entrada.component';
-import { ClsSocialAPIService } from '../../servicos/social_API/clsSocialAPI.service';
+import { Component } from '@angular/core';
+import { EntrarComponent } from './entrar/entrar.component';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, ReactiveFormsModule, CreEntradaComponent],
+  imports: [EntrarComponent],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-  //#region propriedades
-  fgLoginForm!: FormGroup;
-  usuario!: tUsuario ;
-  //#endregion
+export class LoginComponent {
 
-  flgSubmit: boolean = false;
-
-  constructor(private fbLoginForm: FormBuilder, private socialAPI: ClsSocialAPIService) {}
-
-  //#region eventos
-  //classe
-  ngOnInit(): void { this.preparaForm() }
-  //objetos
-  onSubmitForm(): void { this.enviaLogin() }
-  onClick(): void { this.enviaLogin() }
-  //#endregion
-
-  //#region metodos
-  preparaForm(): void {
-    //iniciando
-    this.usuario = {email: '', nome: '', senha: ''}
-    //configura formulario
-    this.fgLoginForm = this.fbLoginForm.group({
-      email: new FormControl (this.usuario.email, [
-        Validators.required,    //obrigatorio
-        Validators.email,   //validacoes de email
-        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')   //formato obrigatorio do dado: ____@____.____
-      ]),
-      senha: new FormControl (this.usuario.senha, [
-        Validators.required,
-        Validators.minLength(8)   //tamanho minimo da senha
-      ])
-    });
-  }
-
-  get getEmail() {
-    return this.fgLoginForm.get('email');
-  }
-  get getSenha() {
-    return this.fgLoginForm.get('senha');
-  }
-
-  get getForm() {
-    return this.fgLoginForm.controls;
-  }
-
-  validaDados(): boolean {
-    //controle submit
-    this.flgSubmit = true;
-    //retorna verdadeiro caso formulario esteja valido
-    return this.fgLoginForm.valid
-  }
-
-  enviaLogin(): void {
-    //---------
-    if (this.validaDados()) {
-      // Atualiza os dados do formulário
-      this.usuario.email = this.getEmail?.value;
-      this.usuario.senha = this.getSenha?.value;
-      // Efetua a requisição de login
-      this.socialAPI.executaLogin(this.usuario.email, this.usuario.senha)
-        .subscribe({
-          next: () => {
-            // Reseta os dados e o formulário
-            this.usuario = { email: '', nome: '', senha: '' };
-            this.flgSubmit = false;
-            this.fgLoginForm.reset();
-          },
-          error: (erros) => {
-            //validacoes
-            if (erros.message.includes('already exists')) {
-              //email duplicado
-              this.fgLoginForm.get('email')?.setErrors({ emailDuplicado: true });
-            }
-          }
-        })
-    }
-  }
-  //#endregion
 }

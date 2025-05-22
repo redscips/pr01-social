4bae44949cabf64605602c59dad777f84d897e84
redscips <redscipher@gmail.com>
2025-05-21 22:22:09 -0300

    feat: subindo nova pagina

--- frente-social/src/app/app.config.ts
+++ frente-social/src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter, withRouterConfig } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations'
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 //configura o roteador p/ recarregar a rota mesmo que a URL seja a mesma
@@ -13,6 +14,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, roteadorConfig),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    provideAnimations()
   ]
 };
--- frente-social/src/app/componentes/mural/inicial/inicial.component.ts
+++ frente-social/src/app/componentes/mural/inicial/inicial.component.ts
@@ -1,13 +1,60 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, HostBinding  } from '@angular/core';
+import { CabecalhoComponent } from '../../cabecalho/cabecalho.component';
+import { RodapeComponent } from '../../rodape/rodape.component';
+import { trigger, transition, style, animate } from '@angular/animations'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inicial',
-  imports: [],
+  imports: [CabecalhoComponent, RodapeComponent, CommonModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './inicial.component.html',
-  styleUrl: './inicial.component.scss'
+  styleUrl: './inicial.component.scss',
+  animations: [
+    trigger('aparecer', [
+      //define a transicao quando o elemento eh inserido no DOM
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(30px)' }), //estado inicial
+        animate('1s ease-in-out', style({ opacity: 1, transform: 'translateY(0)' })) //estado final
+      ])
+    ])
+  ]
 })
 export class InicialComponent {
+  //#region variaveis/instancias
+  //array com os caminhos das imagens do slide de fundo
+  imagens: string[] = [
+    '../../../../assets/imagens/oceano.jpg',
+    '../../../../assets/imagens/rio.jpg',
+    '../../../../assets/imagens/arvores.jpg',
+    '../../../../assets/imagens/montanha.jpg',
+    '../../../../assets/imagens/region.jpg'
+  ];
+  //var: indice da imagem atual
+  ImagemAtual: number = 0;
+  tempo: number = 60000;
+  //
+  //#endregion
 
-  noClique(evento: any): void { alert(evento.detail.mensagem) }
+  //#region propriedades
+  //amarra do style.backgroundImage ao host
+  @HostBinding('style.backgroundImage')
+  get backgroundImage(): string {
+    return `url(${this.imagens[this.ImagemAtual]})`;    //retorna imagem do slide atual p/ o fundo do 'host'
+  }
+  //#endregion
+
+  //#region eventos
+  ngOnInit(): void { this.trocaImagemSlide() }
+  //#endregion
+
+  //#region metodos
+  //
+  trocaImagemSlide() {
+    setInterval(() => {
+      //passa p/ proxima imagem
+      this.ImagemAtual = (this.ImagemAtual + 1) % this.imagens.length;
+    }, this.tempo);   //troca de imagem a cada 5 segundos
+  }
+  //#endregion
 }

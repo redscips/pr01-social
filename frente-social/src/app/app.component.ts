70753dd61b7b0f4a7dd49c88854117e7b2f4804b
redscips <redscipher@gmail.com>
2025-05-29 21:11:43 -0300

    chore: fantasma

--- frente-social/src/app/app.component.ts
+++ frente-social/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { ClsSocialAPIService } from './servicos/social_API/clsSocialAPI.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,13 @@ import { ClsSocialAPIService } from './servicos/social_API/clsSocialAPI.service'
 export class AppComponent {
   title = 'Rede Social';
 
-  constructor (private socialAPI: ClsSocialAPIService) { }
+  flgExibe = false;
+
+  constructor (
+    private socialAPI: ClsSocialAPIService,
+    private roteador: Router,
+    @Inject(PLATFORM_ID) private plataformaID: any
+  ) { }
 
   //#region eventos: classe
   ngOnInit(): void { this.preparaForm() }
@@ -19,8 +26,23 @@ export class AppComponent {
 
   //#region metodos
   preparaForm(): void {
-    //configura o token p/ acessar
+    //forca redesenhar as paginas: p/ evitar de aparecer o flash/imagem fantasma
+    // do componente na memoria anterior a algum evento de recarga
+    //if (isPlatformBrowser(this.plataformaID))
+    document.body.offsetHeight;   //gatilho reflow
+
+    //configura o token p/ acessa
     this.socialAPI.validaToken()
+
+    // Só mostra conteúdo depois da primeira navegação completar
+    this.roteador.events.subscribe(evento => {
+      if (evento instanceof NavigationEnd) {
+        // Pequeno delay para evitar flash
+        setTimeout(() => {
+          this.flgExibe = true;
+        }, 50);
+      }
+    });
   }
   //#endregion
 }
